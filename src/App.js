1c0031445285218600cf981cc3bf97e5ce7dee98
Joe Klakus <joeyk2k@hotmail.com>
2022-10-21 14:26:05 +0200

    map working

--- src/App.js
+++ src/App.js
@@ -38,92 +38,22 @@ function App() {
   return (
     <div className="App">
       <div>Wordle</div>
-      <div className="row">
-        {
-          (wordsArray[0]?.split(' ').map(i => i.charAt(0)).toString() === data[0]?.charAt(0))
-            ?
-            <div className="box" style={{ backgroundColor: "green" }}>{wordsArray[0]?.split(' ').map(i => i.charAt(0))}</div>
-            :
-            (wordsArray[0]?.split(' ').map(i => i.charAt(0)).toString() === data[0]?.charAt(0)) ///TODO LIST
-              ?
-              <div className="box" style={{ backgroundColor: "yellow" }}>{wordsArray[0]?.split(' ').map(i => i.charAt(0))}</div>
-              :
-              <div className="box">{wordsArray[0]?.split(' ').map(i => i.charAt(0))}</div>
+      {wordsArray.map((word, index) => (
+        <div className="row">
+          {word.split('').map((letter, index) => (
+          (letter === data[0]?.charAt(index))
+          ?
+          <div className="box" style={{ backgroundColor: "green" }}>{letter}</div>
+          :
+          (letter === data[0]?.charAt(index)) ///TODO LIST
+          ?
+          <div className="box" style={{ backgroundColor: "yellow" }}>{letter}</div>
+          :
+          <div className="box">{letter}</div>
+          ))}
+        </div>
+          ))
         }
-        {
-          (wordsArray[0]?.split(' ').map(i => i.charAt(1)).toString() === data[0]?.charAt(1))
-            ?
-            <div className="box" style={{ backgroundColor: "green" }}>{wordsArray[0]?.split(' ').map(i => i.charAt(1))}</div>
-            :
-            (wordsArray[0]?.split(' ').map(i => i.charAt(1)).toString() === data[0]?.charAt(1)) ///TODO LIST
-              ?
-              <div className="box" style={{ backgroundColor: "yellow" }}>{wordsArray[0]?.split(' ').map(i => i.charAt(1))}</div>
-              :
-              <div className="box">{wordsArray[0]?.split(' ').map(i => i.charAt(1))}</div>
-        }
-        {
-          (wordsArray[0]?.split(' ').map(i => i.charAt(2)).toString() === data[0]?.charAt(2))
-            ?
-            <div className="box" style={{ backgroundColor: "green" }}>{wordsArray[0]?.split(' ').map(i => i.charAt(2))}</div>
-            :
-            (wordsArray[0]?.split(' ').map(i => i.charAt(2)).toString() === data[0]?.charAt(2)) ///TODO LIST
-              ?
-              <div className="box" style={{ backgroundColor: "yellow" }}>{wordsArray[0]?.split(' ').map(i => i.charAt(2))}</div>
-              :
-              <div className="box">{wordsArray[0]?.split(' ').map(i => i.charAt(2))}</div>
-        }
-        {
-          (wordsArray[0]?.split(' ').map(i => i.charAt(3)).toString() === data[0]?.charAt(3))
-            ?
-            <div className="box" style={{ backgroundColor: "green" }}>{wordsArray[0]?.split(' ').map(i => i.charAt(3))}</div>
-            :
-            (wordsArray[0]?.split(' ').map(i => i.charAt(3)).toString() === data[0]?.charAt(3)) ///TODO LIST
-              ?
-              <div className="box" style={{ backgroundColor: "yellow" }}>{wordsArray[0]?.split(' ').map(i => i.charAt(3))}</div>
-              :
-              <div className="box">{wordsArray[0]?.split(' ').map(i => i.charAt(3))}</div>
-        }
-      </div>
-
-      <div className="row">
-        <div className="box">{wordsArray[1]?.split(' ').map(i => i.charAt(0))}</div>
-        <div className="box">{wordsArray[1]?.split(' ').map(i => i.charAt(1))}</div>
-        <div className="box">{wordsArray[1]?.split(' ').map(i => i.charAt(2))}</div>
-        <div className="box">{wordsArray[1]?.split(' ').map(i => i.charAt(3))}</div>
-        <div className="box">{wordsArray[1]?.split(' ').map(i => i.charAt(4))}</div>
-      </div>
-
-      <div className="row">
-        <div className="box">{wordsArray[2]?.split(' ').map(i => i.charAt(0))}</div>
-        <div className="box">{wordsArray[2]?.split(' ').map(i => i.charAt(1))}</div>
-        <div className="box">{wordsArray[2]?.split(' ').map(i => i.charAt(2))}</div>
-        <div className="box">{wordsArray[2]?.split(' ').map(i => i.charAt(3))}</div>
-        <div className="box">{wordsArray[2]?.split(' ').map(i => i.charAt(4))}</div>
-      </div>
-
-      <div className="row">
-        <div className="box">{wordsArray[3]?.split(' ').map(i => i.charAt(0))}</div>
-        <div className="box">{wordsArray[3]?.split(' ').map(i => i.charAt(1))}</div>
-        <div className="box">{wordsArray[3]?.split(' ').map(i => i.charAt(2))}</div>
-        <div className="box">{wordsArray[3]?.split(' ').map(i => i.charAt(3))}</div>
-        <div className="box">{wordsArray[3]?.split(' ').map(i => i.charAt(4))}</div>
-      </div>
-
-      <div className="row">
-        <div className="box">{wordsArray[4]?.split(' ').map(i => i.charAt(0))}</div>
-        <div className="box">{wordsArray[4]?.split(' ').map(i => i.charAt(1))}</div>
-        <div className="box">{wordsArray[4]?.split(' ').map(i => i.charAt(2))}</div>
-        <div className="box">{wordsArray[4]?.split(' ').map(i => i.charAt(3))}</div>
-        <div className="box">{wordsArray[4]?.split(' ').map(i => i.charAt(4))}</div>
-      </div>
-
-      <div className="row">
-        <div className="box">{wordsArray[5]?.split(' ').map(i => i.charAt(0))}</div>
-        <div className="box">{wordsArray[5]?.split(' ').map(i => i.charAt(1))}</div>
-        <div className="box">{wordsArray[5]?.split(' ').map(i => i.charAt(2))}</div>
-        <div className="box">{wordsArray[5]?.split(' ').map(i => i.charAt(3))}</div>
-        <div className="box">{wordsArray[5]?.split(' ').map(i => i.charAt(4))}</div>
-      </div>
       <div>
         <input type="text" maxLength="5" ref={inputRef} onChange={handleInput} />
         <button onClick={handleSubmit}>Submit</button>
