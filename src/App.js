50b74bab74eca6733ec80d1aaa9f198e8e91a8ec
Jed <jedhumphries@hotmail.co.uk>
2022-10-21 10:25:25 +0100

    fetched random word data

--- src/App.js
+++ src/App.js
@@ -1,7 +1,26 @@
-import logo from './logo.svg';
+
 import './App.css';
+import { useEffect, useState } from 'react';
 
 function App() {
+  const [data, setData] = useState([]);
+  
+  useEffect(() => {
+    // declare the data fetching function
+    const fetchData = async () => {
+      const data = await fetch('https://random-word-api.herokuapp.com/word?length=5');
+      const json = await data.json() 
+      setData(json)
+       console.log(json)
+    }
+  
+    // call the function
+    fetchData()
+   
+      // make sure to catch any error
+      .catch(console.error);
+  }, [])
+
   return (
     <div className="App">
       <div>Wordle</div>
